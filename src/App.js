a3b7442f72e674868287e6537cf5e5c31bcc732c
Luan Argolo <ipsluan@icloud.com>
2021-09-04 00:00:42 -0300

    :sparkles: Finish project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [valuePercentagem, setPercentagem] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <h1>Calculadora de Gorjeta</h1>
+
+      <p>Qual o valor da conta?</p>
+      <input type="number" className="input-value" value={value} onChange={(e) => setValue(parseFloat(e.target.value))}></input>
+
+      <p>Qual a porcentagem da gorjeta?</p>
+      <input type="number" className="input-value" value={valuePercentagem} onChange={(e) => setPercentagem(parseFloat(e.target.value))}></input>
+    
+      <hr/>
+
+      {value > 0 &&
+        <div className="resume">
+          <h1>Resumo da Conta</h1>
+          <p>Valor consumido: R${value}</p>
+          <p>Gorjeta: {valuePercentagem}% = R${(valuePercentagem/100) * value}</p>
+          <p>Valor total a pagar: <strong>R${value + (valuePercentagem/100 * value)}</strong></p>
+        </div>
+      }
     </div>
   );
 }
